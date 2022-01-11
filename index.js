5f95c87b1c54d19af57a754f46b12c6dae43b613
Sojin Kim <sojinjkim@gmail.com>
2022-01-10 16:25:39 -0800

    CryptoWatcher is online! It's able to log on

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Client, Intents } = require("discord.js");
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const { Token } = require("./config.json");
+
+client.once("ready", () => {
+    console.log("CryptoWatcher is online!");
+});
+
+client.login(Token);
